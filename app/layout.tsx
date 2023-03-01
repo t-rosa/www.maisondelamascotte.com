aed99084376154254cc39439a1b7db137b678e66
Toma Rosa <toma.rosa@protonmail.com>
2023-03-01 10:38:18 +0100

    chore: add allura font and setup for dark/light mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Allura } from "next/font/google"
 import { Toaster } from '@/components/ui/toaster'
 import '@/styles/globals.css'
 import { type PropsWithChildren } from 'react'
@@ -6,9 +7,16 @@ export const metadata = {
   title: 'Maison de la mascotte',
 }
 
+const allura = Allura({
+  weight: ['400'],
+  variable: '--font-allura',
+  display: 'swap',
+  subsets: ['latin']
+})
+
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html lang='fr'>
+    <html lang='fr' className={`${allura.variable}`}>
       <body>
         {children}
         <Toaster />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class", '[data-theme="dark"]'],
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        allura: ['var(--font-allura)']
+      }
+    },
   },
   plugins: [
     require('tailwindcss-animate'),
