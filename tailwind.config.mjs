b8d914f9f02b9c7d3f6268c577120a0543c6b80d
Toma Rosa <toma.rosa@ik.me>
2024-07-27 09:48:27 +0200

    chore: add fonts, assets and setup root layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+				title: ["The Nautigal", ...defaultTheme.fontFamily.sans],
+			},
+			colors: {
+				lavande: "#9683EC",
+			},
+		},
 	},
 	plugins: [],
-}
+};
