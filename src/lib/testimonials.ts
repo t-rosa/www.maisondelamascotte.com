6607385b637f3ddb7eeecfc730fcd21e0c90ab43
Toma Rosa <toma.rosa@ik.me>
2024-07-27 16:09:54 +0200

    feat: places list, testimonials, contact and english translation

---
+++ src/lib/testimonials.ts
@@ -0,0 +1,43 @@
+export const testimonials = [
+  {
+    id: 1,
+    text: "Maison au top ! Calme, endroit propice au repos et au dépaysement. Propriétaires très accueillants et discrets. Petites attentions dès l’ arrivée et durant le séjour qui font vraiment plaisir. A refaire absolument! MERCI !",
+    textEn: 'Top house! Quiet, ideal place to relax and get away from it all. Very welcoming and discreet owners. Little touches on arrival and during your stay that really make your day. A must do again! THANK YOU!',
+    author: "Didier",
+    country: "Belgique"
+  },
+  {
+    id: 2,
+    text: "Nous avons été reçu très chaleureusement par les propriétaires. Une maison au calme, très agréable, fonctionnelle, nous nous sommes sentis comme chez nous. Encore un grand merci aux propriétaires pour leur bienveillance.",
+    textEn: "We received a very warm welcome from the owners. A quiet, very pleasant, functional house that made us feel right at home. Many thanks again to the owners for their kindness.",
+    author: "Laura",
+    country: "France"
+  },
+  {
+    id: 3,
+    text: "Nous avons beaucoup aimé la Maison de la Mascotte, sa terrasse et sa piscine privative. La maison était très propre avec tous les équipements nécessaires à des vacances, notamment un barbecue. La maison est bien située pour visiter la Provence, Alpilles, Arles, Avignon et la Camargue. La petite ville de Graveson est à 5 mins en voiture et permet de faire les courses. Par ailleurs, nous avons beaucoup apprécié la gentillesse de nos hôtes. Nous reviendrons avec plaisir!",
+    textEn:"We really liked the Maison de la Mascotte, its terrace and private pool. The house was very clean with all the facilities you need for a vacation, including a barbecue. The house is well located for visiting Provence, Alpilles, Arles, Avignon and the Camargue. The small town of Graveson is a 5-minute drive away and makes shopping easy. We also really appreciated the kindness of our hosts. We'll be back with pleasure!",
+    author: "Julie",
+    country: "France"
+  },
+  {
+    id: 4,
+    text: "La gentillesse et discrétion de l'hôte , l'environnement est splendide pour des vacances reposantes Piscine et extérieur bien entretenus. Très belle maison confortable et décorée avec goût. Emplacement idéal pour passer des vacances en familles, pleins de parcs à thèmes à faire. Commodité",
+    author: "Odette",
+    country: "Belgique"
+  },
+  {
+    id: 5,
+    text: "Le calme du lieu, la gentillesse et la discrétion des propriétaires, rien à redire excellent séjour.",
+    textEn: "The kindness and discretion of the host, the splendid setting for a relaxing vacation. Well-kept pool and grounds. Beautiful, comfortable and tastefully decorated house. Ideal location for a family vacation, lots of theme parks to visit. Convenience",
+    author: "Philippe",
+    country: "France"
+  },
+  {
+    id: 6,
+    text: "Tout était en très bon état et bien équipé. Machine à café, lave-vaisselle, lave-linge, plaques à induction, four, réfrigérateur et congélateur, etc. Draps et serviettes à changer, tout y est. L'emplacement est parfait pour les excursions et la tranquillité absolue et la piscine à usage privé sont un rêve. Les hôtes vivent dans la maison voisine mais vous avez une intimité totale. Hôtes super sympas Merci à Nathalie et Franck",
+    textEn: "Everything was in very good condition and well equipped. Coffee machine, dishwasher, washing machine, induction hob, oven, fridge and freezer, etc. Sheets and towels to be changed, everything is there. The location is perfect for excursions, and the absolute tranquility and private pool are a dream. The hosts live in the house next door but you have total privacy. Super friendly hosts Thanks to Nathalie and Franck.",
+    author: "Sabine",
+    country: "Allemagne"
+  },
+];
